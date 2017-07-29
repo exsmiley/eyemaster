985ad0e53111a5ad82308d024b6083bca9dbdb60
William Papper <contact@papper.me>
2017-07-29 15:39:08 -0700

    Added selected items for rotation purposes

--- css-test/script.js
+++ css-test/script.js
@@ -1,7 +1,15 @@
 function onBlink() {
-  jquery.find(".selected")
+  var selected = $.find(".selected")[0].id
+  selectedArray = ["one", "two", "three", "four"]
+  $(".selected").removeClass("selected")
+  for (item in selectedArray) {
+    if (selectedArray[item] === selected) {
+      $("." + selectedArray[item + 1]).addClass("selected")
+    }
+  }
 }
 
   $(document).ready(function(){
     $(document).bind('keydown', 'shift+4', function(){$('.quadrant').removeClass('clear');}).bind('keyup','shift+4', function(){$('.quadrant').addClass('clear');});
+    onBlink();
   })
