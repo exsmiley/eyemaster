d5404254707c1f6090a02487bb031fb60cd32d42
ongkong <yiong@hmc.edu>
2017-07-29 16:02:12 -0700

    fxns on keyup based on section

--- css-test/script.js
+++ css-test/script.js
@@ -1,3 +1,19 @@
+selectedArray = ["one", "two", "three", "four"]
+function applyFuncBasedOnSelected(arrayOfFunc){
+  var selected = $.find(".selected")[0].id;
+  for (i = 0; i < 4; i++){
+    if (selectedArray[i] == selected){
+      var func_to_execute = arrayOfFunc[i];
+      func_to_execute();
+      return true
+    }
+  }
+}
+function ex(num){
+  return function f(){
+    console.log(num)
+  }
+}
 function onBlink() {
   var selected = $(".selected").removeClass("selected")
   if (selected.next() && selected.next().length) {
@@ -8,7 +24,12 @@ function onBlink() {
   }
 }
 
-  $(document).ready(function(){
-    $(document).bind('keydown', 'shift+4', function(){$('.quadrant').removeClass('clear');}).bind('keyup','shift+4', function(){$('.quadrant').addClass('clear');});
-    onBlink();
-  })
+$(document).ready(function(){
+    $(document).bind('keydown', 'shift+4', function(){$('.quadrant').removeClass('clear');})
+    .bind('keyup','shift+4', function(){
+        $('.quadrant').addClass('clear');
+        applyFuncBasedOnSelected([ex(1),ex(2),ex(3),ex(4)]);
+        
+    });
+    $(document).bind('keydown', 'right', function(){onBlink();});
+})
