677c6d2b73f241648d7135a4448f26a5928adf82
Zachery Miranda <zmiranda@mit.edu>
2017-07-29 15:57:21 -0700

    can open a new tab on blink but commented out in callback

--- ext/src/actions.js
+++ ext/src/actions.js
@@ -1,4 +1,4 @@
 function openNewTab() {
 	// chrome.tabs.create({})
-	chrome.runtime.sendMessage({ from: 'content_script', message: 'Information from webpage.' });
+	chrome.runtime.sendMessage("newTab");
 }
