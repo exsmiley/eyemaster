2d01b80719a95494d95f3500786601214848b14a
ongkong <yiong@hmc.edu>
2017-07-29 20:06:05 -0700

    added text on quadrants

--- ext/src/inject/script.js
+++ ext/src/inject/script.js
@@ -1,4 +1,6 @@
-selectedArray = ["one", "two", "three", "four"]
+selectedArray = ["one", "two", "three", "four"];
+functionsArray = [openNewTab,duplicateTab,reloadTab,removeTab];
+namingArray = ['New Tab', 'Duplicate Tab', 'Reload Tab', 'Remove Tab'];
 function applyFuncBasedOnSelected(arrayOfFunc){
   var selected = $.find(".selected")[0].id;
   for (i = 0; i < 4; i++){
@@ -24,11 +26,18 @@ function onBlink() {
   $('#' + selectedArray[index+1]).addClass('selected')
 }
 
+function nameQuadrants(namingArray){
+  for (i=0; i<4; i++){
+    console.log(namingArray[i]);
+    $('#' + selectedArray[i]).find('.quadrant-text').text(namingArray[i]);
+  }
+}
+
 $(document).ready(function(){
     $(document).bind('keydown', 'shift+4', function(){$('.quadrant').removeClass('clear');})
     .bind('keyup','shift+4', function(){
         $('.quadrant').addClass('clear');
-        applyFuncBasedOnSelected([openNewTab,duplicateTab,reloadTab,removeTab]);
+        applyFuncBasedOnSelected(functionsArray);
     })
     .bind('keydown', 'shift+right', function(){onBlink();});
 })
