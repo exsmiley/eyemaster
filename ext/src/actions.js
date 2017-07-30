ddc53b882f147f2318a6b2110c7dd7fb6bdfc157
Zachery Miranda <zmiranda@mit.edu>
2017-07-29 17:45:46 -0700

    fixed weird error with jquery not being able to be found on src and move face loading to function

--- ext/src/actions.js
+++ ext/src/actions.js
@@ -25,3 +25,45 @@ function googleIt(selectedText) {
   var url = "https://www.google.com/search?q=" + selectedText;
   chrome.runtime.sendMessage({"action": "googleIt", "url": url});
 }
+
+function loadVideo() {
+	var width = 320;
+    var height = 240;
+    var topDist = '0px';
+    var leftDist = '0px';
+
+    var video = document.getElementById('webgazerVideoFeed');
+    video.style.display = 'block';
+    video.style.position = 'absolute';
+    video.style.top = topDist;
+    video.style.left = leftDist;
+    video.width = width;
+    video.height = height;
+    video.style.margin = '0px';
+
+    webgazer.params.imgWidth = width;
+    webgazer.params.imgHeight = height;
+
+    var overlay = document.createElement('canvas');
+    overlay.id = 'overlay';
+    overlay.style.position = 'absolute';
+    overlay.width = width;
+    overlay.height = height;
+    overlay.style.top = topDist;
+    overlay.style.left = leftDist;
+    overlay.style.margin = '0px';
+
+    document.body.appendChild(overlay);
+
+    var cl = webgazer.getTracker().clm;
+
+    function drawLoop() {
+        requestAnimFrame(drawLoop);
+        overlay.getContext('2d').clearRect(0,0,width,height);
+        if (cl.getCurrentPosition()) {
+            cl.draw(overlay);
+        }
+    }
+    drawLoop();
+    textToSpeech("Face. AWAKEN!", "Bad News")
+};
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -45,51 +45,9 @@ window.addEventListener("load", function() {
         .begin()
         .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
 
-    var width = 320;
-    var height = 240;
-    var topDist = '0px';
-    var leftDist = '0px';
-    
-    var setup = function() {
-        var video = document.getElementById('webgazerVideoFeed');
-        video.style.display = 'block';
-        video.style.position = 'absolute';
-        video.style.top = topDist;
-        video.style.left = leftDist;
-        video.width = width;
-        video.height = height;
-        video.style.margin = '0px';
-
-        webgazer.params.imgWidth = width;
-        webgazer.params.imgHeight = height;
-
-        var overlay = document.createElement('canvas');
-        overlay.id = 'overlay';
-        overlay.style.position = 'absolute';
-        overlay.width = width;
-        overlay.height = height;
-        overlay.style.top = topDist;
-        overlay.style.left = leftDist;
-        overlay.style.margin = '0px';
-
-        document.body.appendChild(overlay);
-
-        var cl = webgazer.getTracker().clm;
-
-        function drawLoop() {
-            requestAnimFrame(drawLoop);
-            overlay.getContext('2d').clearRect(0,0,width,height);
-            if (cl.getCurrentPosition()) {
-                cl.draw(overlay);
-            }
-        }
-        drawLoop();
-        textToSpeech("Face. AWAKEN!", "Bad News")
-    };
-
     function checkIfReady() {
         if (webgazer.isReady()) {
-            setup();
+            loadVideo();
         } else {
             setTimeout(checkIfReady, 100);
         }
@@ -124,6 +82,7 @@ $(document).ready(function(){
        
 });
 
+// comment out after we're done training
 window.onbeforeunload = function() {
 	webgazer.end()
 }
